da8e8aa7596ea5f98bff0b81b7c7c1a315474874
Chatur kaur <36239738+chaturk0@users.noreply.github.com>
2025-03-31 12:05:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,3 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
         document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
     });
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Load Header
-    fetch("header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("header-container").innerHTML = data;
-        });
-
-    // Load Footer
-    fetch("footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("footer-container").innerHTML = data;
-        });
-});
-
